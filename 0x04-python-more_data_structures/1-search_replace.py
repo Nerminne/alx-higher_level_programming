cbf79b3a9cdaf5341fd3828bf14959d930441fca
Nerminne <nerminee.mostafaa@gmail.com>
2023-11-08 17:00:29 -0800

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if (len(my_list) == 0):
+        return (None)
     new_list = my_list.copy()
     index = new_list.index(search)
     new_list.remove(search)
