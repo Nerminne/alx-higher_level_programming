0c0425b273930842a09764ecc9a8bd3bdc45f6bd
Nerminne <nerminee.mostafaa@gmail.com>
2023-11-21 18:42:35 -0800

    getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+create class Square with
+Private instance attribute: size
+Instantiation with optional size: size=0
+Public instance method: def area(self) which return area
+"""
+
+
+class Square:
+    """
+    Args:
+        size -> must be integer
+    Methods:
+        __init__(self, size=0)
+        def size(self) -> getter
+        def size(self, value) -> setter
+        area(self) -> public instance method return area
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if (isinstance(value, int)):
+            if (value < 0):
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return (self.__size ** 2)
