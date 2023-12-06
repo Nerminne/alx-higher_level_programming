86d5a70e6bcdfe0051febf4a8b35bb88654315f7
Nerminne <nerminee.mostafaa@gmail.com>
2023-12-05 16:13:19 -0800

    serialization

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function that returns the JSON representation
+of an object (string)
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """ convert python data structure to JSON """
+    json_str = json.dumps(my_obj)
+    return (json_str)
