7d8ae9f963bf580efb8ff89891b52cf8e00fa6f7
Nerminne <nerminee.mostafaa@gmail.com>
2023-12-05 16:39:24 -0800

    deserialization from file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function that creates an Object from a “JSON file”
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """ store converted python data structure in object """
+     with open(filename, "w") as json_file:
+         py_object = json.load(json_file)
+         return (py_object)
