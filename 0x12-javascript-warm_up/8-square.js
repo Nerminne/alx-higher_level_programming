b763b75eb0fe3ee5a5dd1904aa98639edcefe0f1
Nerminne <nerminee.mostafaa@gmail.com>
2024-02-12 18:58:34 -0800

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 
 const num = parseInt(process.argv[2]);
 if (isNaN(num)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let i = 0; i < num; i++) {
     console.log('X'.repeat(num));
