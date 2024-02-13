08acfd233802ca336b48ca6e76a3f12f62af8644
Nerminne <nerminee.mostafaa@gmail.com>
2024-02-12 18:19:03 -0800

    first 7 tasks

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 
-console.log(isNaN(Number(process.argv[2])) ? "Not a number" : `My number: ${Number(process.argv[2])}`);
+console.log(isNaN(Number(process.argv[2])) ? 'Not a number' : `My number: ${Number(process.argv[2])}`);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let str = ["C is fun", "Python is cool", "JavaScript is amazing"], i = 0;
+const str = ['C is fun', 'Python is cool', 'JavaScript is amazing']; let i = 0;
 while (i < str.length) {
-	console.log(str[i++]);
+  console.log(str[i++]);
 }
