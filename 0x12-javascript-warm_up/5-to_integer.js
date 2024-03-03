4039259602a05714bacec9d566f40689b16655da
Nerminne <138694722+Nerminne@users.noreply.github.com>
2024-03-03 12:39:57 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-
-console.log(isNaN(Number(process.argv[2])) ? 'Not a number' : `My number: ${Number(process.argv[2])}`);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-const str = ['C is fun', 'Python is cool', 'JavaScript is amazing']; let i = 0;
-while (i < str.length) {
-  console.log(str[i++]);
-}
