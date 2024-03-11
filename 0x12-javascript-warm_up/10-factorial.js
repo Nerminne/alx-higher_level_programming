e23a9dfc1c66b4308a608314be13daaf2fea9eb6
Nerminne <nerminee.mostafaa@gmail.com>
2024-03-11 15:53:18 -0700

    fact

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
-const num = process.argv[2] || 1; let fact = 1;
-for (let i = num; i > 0; i--) {
-  fact *= i;
+function factorial (num) {
+    let fact = 1;
+    for (let i = num; i > 0; i--) {
+    fact *= i;
+  }
+  return (fact);
 }
+let fact = factorial(process.argv[2] || 1);
 console.log(fact);
