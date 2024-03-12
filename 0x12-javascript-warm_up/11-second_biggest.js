1a7160799728b5a7928130d54828caaa742cb4dc
Nerminne <nerminee.mostafaa@gmail.com>
2024-03-11 17:24:57 -0700

    second biggest num

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+let num;
+if (process.argv.length <= 3) {
+  num = 0;
+} else {
+  const arr = process.argv.slice(2).map(Number);
+  for (let i = 0; i < arr.length; i++) {
+    if (isNaN(arr[i])) {
+      arr[i] = 0;
+    }
+  }
+  let index = arr.indexOf(Math.max(...arr));
+  arr[index] = 0;
+  num = Math.max(...arr);
+}
+console.log(num);
