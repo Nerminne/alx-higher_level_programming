c671165cbf57360f3007ede563ffa23deb267a4e
Nerminne <nerminee.mostafaa@gmail.com>
2024-03-12 17:58:38 -0700

    rect/sqr

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Square_ = require('./5-square');
+
+module.exports = class Square extends Square_ {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('C'.repeat(this.height));
+      }
+    }
+  }
+};
