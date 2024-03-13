f2ce1e3389d4a303b383f5dffca5ef0a4ffb4a68
Nerminne <nerminee.mostafaa@gmail.com>
2024-03-12 18:04:37 -0700

    rect/sqr

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-const BaseSquare = require('./5-square');
+const ParentSquare = require('./5-square');
 
-module.exports = class Square extends BaseSquare {
+module.exports = class Square extends ParentSquare {
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
       for (let col = 0; col < this.width; col += 1) {
-	console.log(c.repeat(this.height));
+        console.log(c.repeat(this.width));
       }
     }
   }
