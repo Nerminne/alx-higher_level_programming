303e73fbae011c360dd8dc196776a02c682ae64d
Nerminne <nerminee.mostafaa@gmail.com>
2024-03-12 18:55:15 -0700

    task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const list = require('./100-data').list;
-const new_list = list.map(function (value, index) {
+const newList = list.map(function (value, index) {
   return value * index;
 });
 
 console.log(list);
-console.log(new_list);
+console.log(newList);
