c5e0fe22ea665cf6493035f982a2de683d42fb0b
Nerminne <nerminee.mostafaa@gmail.com>
2024-04-12 00:00:45 -0700

    content len

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Bash script that displays the size of the body of the response
 
-curl -sI "$1" | grep 'Content-Length: ' | tail -c +17
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
