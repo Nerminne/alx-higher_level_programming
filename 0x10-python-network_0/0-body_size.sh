0ebd75cc1c0689c1f824f3f638c88d6bff4b8889
Nerminne <138694722+Nerminne@users.noreply.github.com>
2024-04-13 09:25:59 +0200

    exp

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Bash script that displays the size of the body of the response
-
-curl -sI "$1" | grep 'Content-Length: ' | cut -f2 -d' '
+curl -sI "$1" | grep 'Content-Length: ' | tail -c +17
