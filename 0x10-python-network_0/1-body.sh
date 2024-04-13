81bdb6f62096d69ce430b64cf8b3fcfced1b818c
Nerminne <138694722+Nerminne@users.noreply.github.com>
2024-04-13 08:50:53 +0200

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #script that takes in a URL, and displays the body of the response
-#script that takes in a URL, and displays the body of the response
-d=$(curl -sI "$1")
+
+d=$(curl -sL "$1")
 if echo "$d" | grep -q "200 OK"; then
   curl -s "$1"
 fi
